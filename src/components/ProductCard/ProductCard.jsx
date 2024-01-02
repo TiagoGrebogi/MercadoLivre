9b94495d621df4aabce6c340535d2f6427f57b3f
ocachorro <tiagong2005@gmail.com>
2024-01-01 23:42:50 -0300

    commit

---
+++ src/components/ProductCard/ProductCard.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function ProductCard() {
+  return (
+    <section className="product-card">
+      product card
+    </section>
+    
+  );
+}
+
+export default ProductCard;
